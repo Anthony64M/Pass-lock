213e8fce87e62243d0517d34efad0c3d7a3d2db1
Anthony64M <anthony.mutuku@student.moringaschool.com>
2021-09-03 13:02:08 +0300

    The function saves new user information

--- password_locker.py
+++ password_locker.py
@@ -1,10 +1,15 @@
 import pyperclip
 from user_credentials import User, Credentials
 
-def create_User(fname,lname,password):
+def create_user(fname,lname,password):
     '''
     This Function creates a new user account
     '''
     new_user = User(fname,lname,password)
     return new_user
 
+def save_user(user):
+    '''
+    This Function saves new user information
+    '''
+    User.save_user(user)
