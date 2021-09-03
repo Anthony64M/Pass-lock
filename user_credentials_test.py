3055825264da10b3147da22893e85d0a94b001e9
Anthony64M <anthony.mutuku@student.moringaschool.com>
2021-09-03 21:13:56 +0300

    Updated the test.py code

--- user_credentials_test.py
+++ user_credentials_test.py
@@ -62,4 +62,62 @@ def test__init__(self):
         """
         Test to if check the initialization/creation of credential instances is properly done.
         """
-               
+        self.assertEqual(self.new_credential.user_name,"Anthony")
+        self.assertEqual(self.new_credential.site_name,"Instagram")
+        self.assertEqual(self.new_credential.account_name,"jymal_anthony")
+        self.assertEqual(self.new_credential.password,"K8ddj6m2l")
+
+def test_save_credentials(self):
+        """
+        Test to check if the new credential info is saved into the credentials list.
+        """
+        self.new_credential.save_credentials()
+        pinterest = Credentials('Jymal','Pinterest','Jymal64','K8ddj6m2l')
+        pinterest.save_credentials()
+        self.assertEqual(len(Credentials.credentials_list),2)
+
+def tearDown(self):
+        """
+        Function to clear the credentials list after every test
+        """
+        Credentials.credentials_list = [] 
+        User.users_list = []
+
+def test_display_credentials(self):
+    """
+    Test to check if the display_credentials method, displays the correct credentials.
+    """
+    self.new_credential.save_credentials()
+    pinterest = Credentials('Jymal','Pinterest','Jymal64','K8ddj6m2l')
+    pinterest.save_credentials()
+    gmail = Credentials("Jymal","Gmail","Jymal","K8ddj6m2l")
+    gmail.save_credentials()
+    self.assertEqual(len(Credentials.display_credentials(pinterest.user_name)),2)
+
+def test_find_by_site_name(self):
+    """
+    Test to check if the find_by_site_name method returns the correct credential.
+    """
+    self.new_credential.save_credentials()
+    pinterest = Credentials('Jymal','Pinterest','Jymal64','K8ddj6m2l')
+    pinterest.save_credentials()
+    credentials_exists = Credentials.find_by_site_name("Pinterest")
+    self.assertEqual(credentials_exists.pinterest)
+
+def test_copy_credential(self):
+    """
+    Test to check if the copy a credential method copies the correct credential.
+    """
+    self.new_credential.save_credentials()
+    pinterest = Credentials('Jymal','Pinterest','Jymal64','K8ddj6m2l')
+    pinterest.save_credentials()
+    find_credential = None
+    for credential in Credentials.user_credential_list:
+                    find_credential = credential.find_by_site_name(credential.site_name)
+                    return pyperclip.copy(find_credential.site_name)
+    Credentials.copy_credentials(self.new_credential.site_name)
+    self.assertEqual("K8ddj6m2l", pyperclip.paste())
+    print(pyperclip.paste())
+
+if __name__ == '__main__':
+        unittest.main(verbosity=2)            
