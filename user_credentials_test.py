8bcc4a6d120b9f0498d0f8003e903f11077b549b
Anthony64M <anthony.mutuku@student.moringaschool.com>
2021-09-03 20:33:14 +0300

    Added the instagram input

--- user_credentials_test.py
+++ user_credentials_test.py
@@ -32,5 +32,34 @@ class TestUser(unittest.TestCase):
             
 class TestCredentials(unittest.TestCase):
     """
-    
+    Test class that defines test cases for the credentials class behaviours.
+
+    Args:
+        unittest.TestCase: helps in creating test cases
     """
+    def test_check_user(self):
+        """
+        Function to test whether the login in function check_user works as expected.
+        """
+        self.new_user = User("Anthony", "Mu\tu\ku","K8ddj6m2l")
+        self.new_user.save_user()
+        user2 = User("Jymal", "An\tho\ny", "K8ddj6m2l")
+        user2.save_user()
+
+        for user in User.users_list:
+                if user.first_name == user2.first_name and user.password == user2.password:
+                        current_user = user.first_name
+        return current_user
+
+        self.assertEqual(current_user,Credential.check_user(user2.password,user2.first_name))
+
+def setUp(self):
+    """
+    Function to create an account's credentials before each test.
+    """
+    self.new_credential = Credentials("Anthony", "Instagram", "jymal_anthony", "K8ddj6m2l")
+def test__init__(self):
+        """
+        Test to if check the initialization/creation of credential instances is properly done.
+        """
+               
